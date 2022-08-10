ed5836de007e177abe69afa11d1a8e55781d998d
Alondrob <alondrob@gmail.com>
2022-08-10 00:29:20 -0400

    8.7.22

--- src/components/forms/Register.jsx
+++ src/components/forms/Register.jsx
@@ -1,16 +1,53 @@
-import Header from "../header/Header"
+import Header from "../header/Header";
 
 const Register = () => {
   return (
-      <div>
-          <Header/>
-          <form>
-              <label>Name</label>
-              {/* <input type="text" onChange={(e) => setName(e.value)}/> */}
-            //  **************break****************
-          </form>
-    </div>
-  )
-}
+    <div className=" h-screen bg-vacation-pattern">
+      <Header />
 
-export default Register
+      <div className="flex justify-center mt-8 mb-6">
+        <input
+          className=" block text-center italic  hover:bg-red-200 font-bold rounded-full border-2 border-sky-900 w-80 h-12 space-y-4"
+          placeholder="Name..."
+          type="text"
+          value={null}
+        />
+      </div>
+      {/* ****************************** */}
+      <div className="flex justify-center mt-4 mb-6">
+        <input
+          className=" block text-center italic  hover:bg-red-200 font-bold rounded-full border-2 border-sky-900 w-80 h-12 space-y-4"
+          placeholder="Email..."
+          type="email"
+          value={null}
+        />
+      </div>
+      {/* ****************************** */}
+      <div className="flex justify-center mt-4 mb-6">
+        <input
+          className=" block text-center italic  hover:bg-red-200 font-bold rounded-full border-2 border-sky-900 w-80 h-12 space-y-4"
+          placeholder="Password..."
+          type="password"
+          value={null}
+        />
+      </div>
+      {/* ****************************** */}
+      <div className="flex justify-center mt-4 mb-4">
+        <input
+          className=" block text-center italic  hover:bg-red-200 font-bold rounded-full border-2 border-sky-900 w-80 h-12 space-y-4"
+          placeholder="Confirm Password..."
+          type="password"
+          value={null}
+        />
+      </div>
+      {/* ****************************** */}
+      <div className=" flex justify-center mt-10 mb-4">
+        <button className="block  font-extrabold italic text-center rounded-full h-16 w-80 border-6 border-blue-300 bg-gray-500 hover:bg-green-100">
+          Sign-Up
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'vacation-pattern': "url('https://d3hne3c382ip58.cloudfront.net/files/uploads/bookmundi/resized/cmsfeatured/tropical-beach-1509683857-785X440.jpg')",
+      })
+    },
   },
   plugins: [],
 };
